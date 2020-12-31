b138f0cc0ab5ef3dca19a7e7d62b1b6837eedfad
kenta.shimada <tinswitl@gmail.com>
2020-12-31 22:38:16 +0900

    Initial Commit

---
+++ SecondKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecondKadaiApp
+//
+//  Created by 島田剣太 on 2020/12/31.
+//  Copyright © 2020 kenta.shimada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
